5fc36acff54f9507b8265bfc143b0fd10c1eb769
Shubham Baghel <32522297+shubham-baghel@users.noreply.github.com>
2019-10-28 21:47:04 +0530

    Add files via upload

--- book-recommendation-app/api.py
+++ book-recommendation-app/api.py
@@ -1,8 +1,6 @@
 import numpy as np 
 from flask import Flask, request, jsonify
 import pickle
-import model
-
 
 app = Flask(__name__)
 
@@ -20,4 +18,6 @@ def predict(id):
 if __name__ == '__main__':
     with open('book.pkl','rb') as f:
         model = pickle.load(f)
+    with open('booksData.pkl','rb') as d:
+        data = pickle.load(d)
     app.run(port=5000, debug=True)
