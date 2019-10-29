03091b0f322c415079dcad46bd22b8079aa3cb00
Shubham Baghel <32522297+shubham-baghel@users.noreply.github.com>
2019-10-29 09:45:22 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -4,6 +4,8 @@ import pickle
 
 app = Flask(__name__)
 
+# model = pickle.load(open('book.pkl', 'rb'))
+
 @app.route('/', methods=['GET'])
 def hello():
     return "Welcome to Book Recommendation App."
@@ -11,13 +13,26 @@ def hello():
 @app.route('/r/<int:id>/', methods=['GET'])
 def predict(id):
     print(id)
-    obj = model()
-    res = obj.recommendedBooks(id)
-    return jsonify(res)
+    obj = recommendedBooks(model,id)
+    return jsonify(obj)
+
+def recommendedBooks(model_knn, bookIndex, number=6):
+        distances, indices = model_knn.kneighbors(us_canada_user_rating_pivot.iloc[bookIndex,:].values.reshape(1, -1), n_neighbors = number)
+        recommendedlist = []
+        for i in range(0, len(distances.flatten())):
+            if i == 0:
+                print('Recommendations for {0}:\n'.format(us_canada_user_rating_pivot.index[bookIndex]))
+            else:
+                print('{0}: {1}, with distance of {2}:'.format(i, us_canada_user_rating_pivot.index[indices.flatten()[i]], distances.flatten()[i]))
+                recommendedlist.append(us_canada_user_rating_pivot.index[indices.flatten()[i]])
+
+        return recommendedlist
+
 
 if __name__ == '__main__':
     with open('book.pkl','rb') as f:
         model = pickle.load(f)
-    with open('booksData.pkl','rb') as d:
+    with open('bookData.pkl','rb') as d:
         data = pickle.load(d)
-    app.run(port=5000, debug=True)
+        us_canada_user_rating_pivot = data.pivot(index = 'bookTitle', columns = 'userID', values = 'bookRating').fillna(0)
+    app.run(port=5001, debug=True)
